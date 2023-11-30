019bf0c7cad66d50b0856bb73c3a824a0ff0c1e9
Fahima <sfahima1388@gmail.com>
2023-11-30 23:29:43 +0600

    jwt added

--- index.js
+++ index.js
@@ -1,12 +1,18 @@
 const express = require("express");
 const cors = require("cors");
 const app = express();
+const jwt = require("jsonwebtoken");
 const port = process.env.PORT || 5000;
 require("dotenv").config();
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 
 //middlewares
-app.use(cors());
+app.use(
+  cors({
+    origin: ["http://localhost:5173", "https://learn-sphere.web.app"],
+    credentials: true,
+  })
+);
 app.use(express.json());
 
 //mongodb
@@ -26,8 +32,109 @@ async function run() {
     // Connect the client to the server	(optional starting in v4.7)
     // await client.connect();
     // Send a ping to confirm a successful connection
-
+    const userCollection = client.db("LS_DB").collection("users");
     const partnersCollection = client.db("LS_DB").collection("partners");
+    const teacherCollection = client.db("LS_DB").collection("teachers");
+    const feedbackCollection = client.db("LS_DB").collection("feedback");
+    const classCollection = client.db("LS_DB").collection("classes");
+
+    //jwt
+    app.post("/jwt", async (req, res) => {
+      const user = req.body;
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN, {
+        expiresIn: "6h",
+      });
+      res.send({ token });
+    });
+
+    //middlewares
+    const verifyToken = (req, res, next) => {
+      if (!req.headers.authorization) {
+        return res.status(401).send({ message: "forbidden access." });
+      }
+      const token = req.headers.authorization.split(" ")[1];
+      jwt.verify(token, process.env.ACCESS_TOKEN, (error, decoded) => {
+        if (error) {
+          return req.status(401).send({ message: "forbidden access" });
+        }
+        req.decoded = decoded;
+        next();
+      });
+    };
+
+    //admin
+    const verifyAdmin = async (req, res, next) => {
+      const email = req.decoded.email;
+      const query = { email: email };
+      const user = await userCollection.findOne(query);
+      const isAdmin = user?.role === "admin";
+      if (!isAdmin) {
+        return res.status(403).send({ message: "forbidden" });
+      }
+      next();
+    };
+
+    //users
+    app.post("/users", verifyToken, verifyAdmin, async (req, res) => {
+      const user = req.body;
+      const query = { email: user.email };
+      const userExist = await userCollection.findOne(query);
+      if (userExist) {
+        return res.send({ insertedId: null });
+      }
+      const result = await userCollection.insertOne(user);
+      res.send(result);
+    });
+
+    app.get(
+      "/users/admin/:email",
+      verifyToken,
+      verifyAdmin,
+      async (req, res) => {
+        const email = req.params.email;
+        if (email !== req.decoded.email) {
+          return res.status(403).send({ message: "unauthorized access." });
+        }
+        const query = { email: email };
+        const user = await userCollection.findOne(query);
+
+        let admin = false;
+        if (user) {
+          admin = user?.role === "admin";
+        }
+        res.send({ admin });
+      }
+    );
+
+    app.get("/users", async (req, res) => {
+      const result = await userCollection.find().toArray();
+      res.send(result);
+    });
+    //make admin
+    app.patch("/users/admin/:id", async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id) };
+      const updateDoc = {
+        $set: {
+          role: "admin",
+        },
+      };
+      const result = await userCollection.updateOne(filter, updateDoc);
+      res.send(result);
+    });
+
+    //make teacher
+    app.patch("/teachers/:id", async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id) };
+      const updateDoc = {
+        $set: {
+          role: "instructor",
+        },
+      };
+      const result = await teacherCollection.updateOne(filter, updateDoc);
+      res.send(result);
+    });
 
     //partners
     app.get("/partners", async (req, res) => {
@@ -36,7 +143,6 @@ async function run() {
     });
 
     //teachers
-    const teacherCollection = client.db("LS_DB").collection("teachers");
     app.post("/teachers", async (req, res) => {
       const teacherReview = req.body;
       const result = await teacherCollection.insertOne(teacherReview);
@@ -52,14 +158,14 @@ async function run() {
     });
 
     //feedback
-    const feedbackCollection = client.db("LS_DB").collection("feedback");
+
     app.get("/feedback", async (req, res) => {
       const result = await feedbackCollection.find().toArray();
       res.send(result);
     });
 
     //accepted classes
-    const classCollection = client.db("LS_DB").collection("classes");
+
     app.post("/class", async (req, res) => {
       const courses = req.body;
       const result = await classCollection.insertOne(courses);
